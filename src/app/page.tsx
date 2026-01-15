fe6aabd036206d38d9f2649306ef84ae980fe69d
Rodrigo Ramirez <yitodeveloper@gmail.com>
2026-01-15 02:00:04 -0300

    feat: redirect from home to identifier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import { redirect } from 'next/navigation';
 
-export default function Home() {
-  redirect('/identifier');
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const params = new URLSearchParams();
+  
+  Object.entries(searchParams).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((v) => params.append(key, v));
+    } else if (value !== undefined) {
+      params.append(key, value);
+    }
+  });
+
+  const queryString = params.toString();
+  const redirectPath = queryString ? `/identifier?${queryString}` : '/identifier';
+
+  redirect(redirectPath);
 }
